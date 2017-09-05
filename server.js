e76e02961773f8d3bd66b269e7990e2a99298ea6
uhlenkott <uhlenkott@aol.com>
2017-09-05 13:31:46 -0700

    alert added

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-}).listen(port, host, function() {
+}).listen(port, host, function () {
+    alert('listening');
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
