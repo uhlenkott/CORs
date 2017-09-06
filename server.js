5ce6a3ae233a94e28fe1906a4033361a6cc465e3
uhlenkott <uhlenkott@aol.com>
2017-09-06 16:12:53 -0700

    Original Files

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
-
 // Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
 
-//var port = process.env.PORT || 8080; //oRiginal Code
-var port = process.env.PORT || 3000; //which you can run both on Azure or local
 // Grab the blacklist from the command-line so that we can update the blacklist without deploying
 // again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
@@ -41,7 +39,6 @@ cors_proxy.createServer({
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
-}).listen(port, host, function () {
-    
+}).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
